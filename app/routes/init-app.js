d2985071f95c836db21fd1171e0acff2d717de83
Sushanth Reddy <sreddy@dotloop.com>
2015-11-11 15:03:00 -0800

    Small trial.

--- app/routes/init-app.js
+++ app/routes/init-app.js
@@ -1,5 +1,38 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+var fields = [
+  {
+    fieldName: 'firstName',
+    placeHolder: 'First Name'
+  }, {
+    fieldName: 'middleName',
+    placeHolder: 'Middle Name'
+  }, {
+    fieldName: 'lastName',
+    placeHolder: 'Last Name'
+  }, {
+    fieldName: 'birthDate',
+    placeHolder: 'Birth Date'
+  }, {
+    fieldName: 'socialNumber',
+    placeHolder: 'Social security #'
+  }, {
+    fieldName: 'primaryEmailAddress',
+    placeHolder: 'Email Address'
+  }, {
+    fieldName: 'homePhone',
+    placeHolder: 'Home Phone'
+  }, {
+    fieldName: 'cellPhone',
+    placeHolder: 'Cell Phone'
+  }, {
+    fieldName: 'driversLicense',
+    placeHolder: 'Drivers License'
+  }
+];
 
+export default Ember.Route.extend({
+  model: function() {
+    return fields;
+  }
 });
